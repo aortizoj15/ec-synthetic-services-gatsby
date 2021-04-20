9eea73ce8d5206989d3a30d35817b5029ee30bdd
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-04-20 02:15:42 -0700

    Slide show mvp

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import Glide from '@glidejs/glide'
 
 export const query = graphql`
   query {
@@ -20,6 +21,8 @@ export const query = graphql`
   }
 `
 
+new Glide('.glide').mount()
+
 const Gallery = ({ data }) => {
   const files = data.allFile.edges
   const galleryImages = files.filter(file => file.node.name.includes('gallery'))
@@ -36,14 +39,33 @@ const Gallery = ({ data }) => {
   return (
     <Layout>
       <main className="p-5 sm:mx-12 lg:mx-20 text-center">
-        <Img className="max-w-sm mx-auto" fluid={bathtub_compare_1_img.node.childImageSharp.fluid} alt="" />
-        <Img className="max-w-sm mx-auto" fluid={bathtub_compare_2_img.node.childImageSharp.fluid} alt="" />
-        <Img className="max-w-sm mx-auto" fluid={bathtub_compare_3_img.node.childImageSharp.fluid} alt="" />
-        <Img className="max-w-sm mx-auto" fluid={bathtub_compare_4_img.node.childImageSharp.fluid} alt="" />
+        {/* <Img className="max-w-sm mx-auto" fluid={bathtub_compare_4_img.node.childImageSharp.fluid} alt="" />
         <Img className="max-w-sm mx-auto" fluid={bathtub_compare_5_img.node.childImageSharp.fluid} alt="" />
         <Img className="max-w-sm mx-auto" fluid={bathtub_img.node.childImageSharp.fluid} alt="" />
         <Img className="max-w-sm mx-auto" fluid={shower_compare_img.node.childImageSharp.fluid} alt="" />
-        <Img className="max-w-sm mx-auto" fluid={sink_compare_img.node.childImageSharp.fluid} alt="" />
+        <Img className="max-w-sm mx-auto" fluid={sink_compare_img.node.childImageSharp.fluid} alt="" /> */}
+        <div class="glide">
+          <div class="glide__track" data-glide-el="track">
+            <div class="glide__arrows" data-glide-el="controls">
+              <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+              <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+            </div>
+            <ul class="glide__slides">
+              <li class="glide__slide">
+                <Img className="max-w-sm mx-auto" fluid={bathtub_compare_1_img.node.childImageSharp.fluid} alt="" />
+
+              </li>
+              <li class="glide__slide">
+                <Img className="max-w-sm mx-auto" fluid={bathtub_compare_2_img.node.childImageSharp.fluid} alt="" />
+
+              </li>
+              <li class="glide__slide">
+                <Img className="max-w-sm mx-auto" fluid={bathtub_compare_3_img.node.childImageSharp.fluid} alt="" />
+
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
     </Layout >
   )
